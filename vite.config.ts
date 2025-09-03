67ea1a9f24e242ff5b33732aaa03b3dd5fb0c609
weijian000 <92417992+weijian000@users.noreply.github.com>
2025-09-03 23:46:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base:'./',
+    base:'/test/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
